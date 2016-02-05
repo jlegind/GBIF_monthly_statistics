d2fb53933f0d0145aa072be770a0f934c14cd035
Jan Legind <jlegind@gbif.org>
2016-02-05 10:28:48 +0100

    Initial commit

--- hive_stats.py
+++ hive_stats.py
@@ -1,28 +1,26 @@
+"""Makes the connection object and runs the query using the cursor"""
 import pyhs2
 
 
 def make_connection(delimiter, host, user, pw, database):
 
     conn = pyhs2.connect(host=host,
-                   user=user,
-                   database=database,
-                   password=pw,
-                   port=10000,
-                   authMechanism="PLAIN")
+                           user=user,
+                           database=database,
+                           password=pw,
+                           port=10000,
+                           authMechanism="PLAIN")
 
     cur = conn.cursor()
-        # Show databases
-    print cur.getDatabases()
+    # Show databases
+    #print cur.getDatabases()
     return cur, conn
 
 
 def run_query(sql, cur, conn):
-            print sql[1]
-            sql = sql[0] % sql[1]
-            print sql
-            cur.execute(sql)
-            # Return column info from query
-            # print cur.getSchema()
-            # Fetch table results
-            yield cur.fetch()
-            # conn.close()
+    """sql comes as a tuple, one part is sql template, the other part is the parameters"""
+    sql = sql[0] % sql[1]
+    print sql
+    cur.execute(sql)
+    # Fetch table results
+    yield cur.fetch()
