ad2debb4dfc7034ff17f875d46cee6ea0348cba8
Jan Legind <jlegind@gbif.org>
2016-02-05 11:54:27 +0100

    Improved documentation

--- launcher.py
+++ launcher.py
@@ -1,16 +1,15 @@
-# The launch module fires off the query prepared in the run_nnnn modules
+"""The launch module fires off the query prepared in the run_nnnn modules
+and writes to csv file. Uses run_query() from the *_stats modules"""
 import hive_stats, postgres_stats, mysql_stats
 import csv
 
 
 def launch(sql, cur, csv_location, conn, statfunc=hive_stats):
-    print csv_location
     hv = statfunc.run_query(sql, cur, conn)
     with open(csv_location, 'w') as csvfile:
         my_writer = csv.writer(csvfile, delimiter='\t')
 
         for j in hv:
-            #print j
             if isinstance(j[0], list):
                 [my_writer.writerow(k) for k in j]
             else:
