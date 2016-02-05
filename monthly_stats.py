2f20fbbcce152be037a25ded4050918aa159c8f0
Jan Legind <jlegind@gbif.org>
2016-02-05 11:39:40 +0100

    New version of monthly_stats starter file

--- monthly_stats.py
+++ monthly_stats.py
@@ -1,10 +1,35 @@
-import run_postgres, run_hive, run_mysql
+import run_postgres, run_hive, run_mysql, csv
+import merge_csvs as mcsv
+#import pandas_test as pt
 
+
+start_date = '2016-01-01'
+end_date = '2016-01-31'
+csv_list = []
+headers = []
+
+##The total downloads stats part
 pg_csv = "/home/jan/Documents/test_postgres.csv"
-# csv_list = []
-runner = run_postgres.pg_stats('2015-01-01', '2015-12-31', pg_csv)
-# # csv_list.append(runner)
+runner = run_postgres.pg_stats(start_date, end_date, pg_csv)
+print runner, "----------------list-------------"
+f = open(runner, "r+")
+rows = []
+labels = ['Total downloaded records', 'users', 'downloads']
+headers.append(['Overall download stats\n %s to %s' % (start_date, end_date),'Counts'])
+#Assigns row labels
+labels.reverse()
+for row in csv.reader(f):
+    #print row
+    rows.append([labels.pop()+"\t"+row[0]])
+f = open(runner, "w+")
+w = csv.writer(f)
+w.writerows(rows)
+f.close()
+csv_list.append(runner)
+#print csv_list
+##END
 
+##Begin User Download for MySQL and Postgres
 my_csv = "/home/jan/Documents/test_mysql.csv"
 runner = run_mysql.my_stats(my_csv)
 
@@ -13,10 +38,10 @@ runner = run_postgres.pg_stats('','', my_csv, sql=("CREATE UNLOGGED TABLE jan_te
                                                    "(user_name varchar(80),"
                                                    "country varchar(150),"
                                                    "iso_code char(5))"))
-#runner = run_postgres.pg_stats('','', my_csv, sql="SELECT * FROM jan_temp")
+
 f = open('/home/jan/Documents/test_mysql.csv', 'rw')
 run_postgres.cur.copy_from(f ,'jan_temp')
-#runner = run_postgres.pg_stats('','', my_csv, sql="SELECT * FROM jan_temp LIMIT 10 ")
+
 sql=("SELECT tmp.country, tmp.iso_code, sum(dod.number_records) AS records_total, count(DISTINCT od.key) AS events, count(DISTINCT od.created_by) AS users "
                 "FROM occurrence_download od "
                 "JOIN dataset_occurrence_download dod ON od.key = dod.download_key "
@@ -24,10 +49,21 @@ sql=("SELECT tmp.country, tmp.iso_code, sum(dod.number_records) AS records_total
                 "WHERE od.status = 'SUCCEEDED' AND od.notification_addresses NOT LIKE '%%@gbif.org' AND date(od.created) BETWEEN %(start)s AND %(end)s "
                 "GROUP BY 1,2 ORDER BY 4 DESC")
 
-runner = run_postgres.pg_stats('2015-01-01','2015-12-31', my_csv, sql=sql)
+headers.append(['Downloads by country from %s to %s\ncountry' % (start_date, end_date), 'iso_code', 'records_total', 'events', 'users'])
+runner = run_postgres.pg_stats(start_date, end_date, my_csv, sql=sql)
+csv_list.append(runner)
+##END
 
-#
+
 hive_csv = "/home/jan/Documents/test_hive%s.csv"
-# runner = run_hive.hive_stats('2015-10-01', '2015-11-30', hive_csv)
-runner = run_hive.hive_stats('2015-01-01', '2015-12-31', hive_csv)
 
+runner = run_hive.hive_stats(start_date, end_date, hive_csv)
+[csv_list.append(j) for j in runner]
+headers.append(['Indexed record_count\nfrom %s to %s' % (start_date, end_date), 'country'])
+headers.append(['Indexed record_count\nTotal up until %s' % (end_date), 'country'])
+
+print csv_list
+print headers
+
+mcsv.into_csv(csv_list, headers)
+#pt.into_csv(csv_list, headers)
