4bb82fc9d2ae5e0202aa888aa284a3fbcce21478
Jan Legind <jlegind@gbif.org>
2020-01-29 16:32:27 +0100

    migrated from psycop to pygres

--- run_postgres.py
+++ run_postgres.py
@@ -1,20 +1,40 @@
 import postgres_stats
-import launcher
 import creds
+import csv
 
-cur, conn = postgres_stats.make_connection(database='prod_b_registry', user=creds.postgres_user, password=creds.postgrespw, host='pg1.gbif.org')
+conn = postgres_stats.make_connection(database='prod_b_registry', host='pg1.gbif.org', user=creds.postgres_user, password=creds.postgrespw)
 
 
-def pg_stats(start, end, csv_file, sql=None):
-        #print type(args)
+def pg_stats(start, end, csv_file, sql=None, labels=None):
+        print(type(start), type(end))
+        # start = start+' {}'.format('00:00:00')
+        # end = end+' {}'.format('00:00:00')
         if sql:
-                sql = (sql, {'start':start, 'end':end})
+                # print("SQL true --- ", sql)
+                sql = sql
         else:
-                sql = (("select sum(d.total_records) from occurrence_download d where date(d.created) BETWEEN %(start)s AND %(end)s and d.status = 'SUCCEEDED' AND d.notification_addresses NOT LIKE '%%@gbif.org' AND created_by != 'nagios' "
+                sql = ("select 'Total records downloaded: ', sum(d.total_records) from occurrence_download d where date(d.created) BETWEEN '{start}' AND '{end}' and d.status = 'SUCCEEDED' AND d.notification_addresses NOT LIKE '%%@gbif.org' AND created_by != 'nagios' "
                         "union all "
-                        "select count(distinct d.created_by) from occurrence_download d where date(d.created) BETWEEN %(start)s AND %(end)s and d.status = 'SUCCEEDED' AND d.notification_addresses NOT LIKE '%%@gbif.org' AND created_by != 'nagios' "
+                        "select 'Total number of users: ', count(distinct d.created_by) from occurrence_download d where date(d.created) BETWEEN '{start}' AND '{end}' and d.status = 'SUCCEEDED' AND d.notification_addresses NOT LIKE '%%@gbif.org' AND created_by != 'nagios' "
                         "union all "
-                        "select count(distinct d.key) from occurrence_download d where date(d.created) BETWEEN %(start)s AND %(end)s and d.status = 'SUCCEEDED' AND d.notification_addresses NOT LIKE '%%@gbif.org' AND created_by != 'nagios' "),
-                       {'start':start, 'end':end})
+                        "select 'Total download events: ', count(distinct d.key) from occurrence_download d where date(d.created)"
+       " BETWEEN '{start}' AND '{end}' and d.status = 'SUCCEEDED' AND d.notification_addresses NOT LIKE '%%@gbif.org' "
+       "AND created_by != 'nagios' ".format(start = start, end=end))
 
-        return launcher.launch(sql, cur, csv_file, conn, statfunc=postgres_stats)
+        # return postgres_stats.run_query(sql, cur, conn)
+        res = postgres_stats.run_query(sql, conn)
+        print(res)
+        print('creating '+csv_file)
+
+        with open(csv_file, 'w+') as csvfile:
+            my_writer = csv.writer(csvfile, delimiter=',', lineterminator='\n')
+            if labels:
+                print('labels TRUE')
+                my_writer.writerow(labels)
+            for j in res:
+                print(j)
+                if isinstance(j[0], list):
+                    [my_writer.writerow(k) for k in j]
+                else:
+                    my_writer.writerow(j)
+        return csv_file
