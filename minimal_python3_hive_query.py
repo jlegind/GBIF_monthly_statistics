352767e858ffc0271f357c62ca1cf1c73c0c0f03
Jan Legind <jlegind@gbif.org>
2020-11-18 16:00:29 +0100

    clean minimal...

--- minimal_python3_hive_query.py
+++ minimal_python3_hive_query.py
@@ -1,17 +1,17 @@
 """Makes the connection object and runs the query using the cursor
-Runs Python 3.6 """
+Runs Python 3.6
+Is a proof-of-concept minimum implementation with a simple SQL statement
+Please uncomment the sql_prod string to test more complex query"""
 from impala.dbapi import connect
 
 #CLI parameters that can be included in bash chron job or other
 # start_date = sys.argv[1]
 
 
-snapshot_yyyymmdd = '20201001'
-
 #takes a yyyymmdd variable as the snapshot occurrence table
 #Compare snapshot_beginning_of_year to current state of prod_h.occurrence or other table by subtracting -
 # - beginning of year numbers.
-
+snapshot_yyyymmdd = '20201001'
 # sql_prod = '''SELECT t1.publishingcountry, t2.publisher_country, t1.ct as Nov11_count, t2.ct as beginning_year, t1.ct - t2.ct as delta FROM
 # (
 # SELECT count(o.gbifid) as ct, publishingcountry FROM prod_h.occurrence o
@@ -25,8 +25,8 @@ snapshot_yyyymmdd = '20201001'
 #{} in string is the variable placeholder for format()
 
 
-connection = connect(host='c5master2-vh',user='jlegind',
-                       database='prod_h', password='jlegind',
+connection = connect(host='c5master2-vh',user='myUser',
+                       database='prod_h', password='myPW',
                        port=10000, auth_mechanism="PLAIN"
                     )
 cursor = connection.cursor()
@@ -35,6 +35,6 @@ test_sql = 'SELECT s.dataset_id, s.publisher_country FROM snapshot.occurrence_20
 
 cursor.execute(test_sql)
 res= cursor.fetchall()
-print('In beta---')
+
 print(res)
 
