e9552a3c2aa3d2b41954218f7b371d2fa8328ccf
Jan Legind <jlegind@gbif.org>
2020-12-02 13:21:03 +0100

    cleanup

--- minimal_python3_hive_query.py
+++ minimal_python3_hive_query.py
@@ -12,21 +12,10 @@ from impala.dbapi import connect
 #Compare snapshot_beginning_of_year to current state of prod_h.occurrence or other table by subtracting -
 # - beginning of year numbers.
 snapshot_yyyymmdd = '20201001'
-# sql_prod = '''SELECT t1.publishingcountry, t2.publisher_country, t1.ct as Nov11_count, t2.ct as beginning_year, t1.ct - t2.ct as delta FROM
-# (
-# SELECT count(o.gbifid) as ct, publishingcountry FROM prod_h.occurrence o
-# GROUP BY publishingcountry
-# )t1
-# JOIN
-# (
-# SELECT count(id) as ct, publisher_country FROM snapshot.occurrence_{}
-# GROUP BY publisher_country)t2
-# ON t1.publishingcountry = t2.publisher_country ORDER BY delta DESC'''.format(snapshot_yyyymmdd)
-#{} in string is the variable placeholder for format()
 
 
-connection = connect(host='c5master2-vh',user='myUser',
-                       database='prod_h', password='myPW',
+connection = connect(host='c5master2-vh',user='jlegind',
+                       database='prod_h', password='jlegind',
                        port=10000, auth_mechanism="PLAIN"
                     )
 cursor = connection.cursor()
